a20072d8aa1bcb090d8dcef9a35d74ac9724dc39
Rafael <rafagallardo75@gmail.com>
2022-03-27 20:22:45 -0600

    back to react again

---
+++ src/PrimeraApp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PrimeraApp = () => {
+  return (
+    <div>Hello World</div>
+  )
+}
+
+export default PrimeraApp
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PrimeraApp from "./PrimeraApp";
+import './index.css'
+
+const saludo = <h1>Hola Mundo</h1>;
+
+const divRoot = document.querySelector('#app');
+
+ReactDOM.render( <PrimeraApp/>, divRoot );
+
+
+
